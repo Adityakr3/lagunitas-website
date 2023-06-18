a42ddb44e2cca40ee57310d5942c63a7813c5a76
Aditya kumar <Adityakumar14022002@gmail.com>
2023-06-18 12:34:53 +0530

    btn and images are added in main body

--- script.js
+++ script.js
@@ -1,36 +1,50 @@
+// ......................................................................................................
 var elem2 = document.querySelector(".elem2");
 var elemDog = document.querySelector(".elemDog");
+elem2.addEventListener("mousemove", function () {
+  elemDog.style.bottom = "-5%";
+  setTimeout(() => {
+    elemDog.style.bottom = "-101%";
+  }, 555);
+});
 
-elem2.addEventListener("mousemove",function(){
-   elemDog.style.bottom="-5%";
-   setTimeout(() => {
-    elemDog.style.bottom="-101%";
-   },555);
-})
-
-
-// ......................slider..........................
+// ......................slider....................................................................
 
 var menu = document.querySelector("#menu");
 var slider = document.querySelector(".lft-slider");
 var close = document.querySelector("#close");
+menu.addEventListener("click", function () {
+  slider.style.left = "0";
+  menu.style.display = "none";
+  close.style.display = "initial";
+});
+close.addEventListener("click", function () {
+  slider.style.left = "-38%";
+  menu.style.display = "initial";
+  close.style.display = "none";
+});
 
-menu.addEventListener("click",function(){
-    slider.style.left="0";
-    menu.style.display='none';
-    close.style.display='initial';
-})
-close.addEventListener("click",function(){
-    slider.style.left="-38%";
-    menu.style.display='initial';
-    close.style.display='none';
-})
-// .......................................overlay ka java script...........................
+// .......................................overlay ka java script..................................
 
 var overlayDiv = document.querySelector("#overlayDiv");
 var close1 = document.querySelector("img");
 var overlay = document.querySelector("#overlay");
+var btn = document.querySelector(".btn-for-fun");
+setTimeout(() => {
+  overlay.style.display = "initial";
+}, 4555);
+close1.addEventListener("click", function () {
+  overlay.style.display = "none";
+});
+btn.addEventListener("click", function () {
+  overlay.style.display = "none";
+});
 
-close1.addEventListener("click",function(){
-   overlay.style.display='none'
-})
+//......................................another sucribe box.........................................
+
+var overDiv = document.querySelector("#overlayDiv1");
+setTimeout(() => {
+  overDiv.style.right = "-1%";
+}, 5955);
+
+//........................................................................................................
